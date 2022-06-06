83ea23db7da27df129c0f29692c75145813c583f
Mostak Ahamed Nishat <37589801+Mostak-Ahamed-Nishat@users.noreply.github.com>
2022-06-06 16:36:38 +0600

    delete and checkMark event added

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const input = document.querySelector('.todo-input');
 const addButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
 
+//Check input is empty or not
 
-
-
+//Event Listener
 addButton.addEventListener("click", function(e) {
     e.preventDefault();
+
+
     //get the input box value
     var text = input.value;
     //create a div inside the ul
@@ -16,22 +18,49 @@ addButton.addEventListener("click", function(e) {
     const newTodo = document.createElement('li')
     newTodo.classList.add('todo-item')
         //Add the input text in the todolist
-    newTodo.innerText = text
+    if (text.length > 3) {
+        newTodo.innerText = text
+
         //append the list with the div
-    todoDiv.appendChild(newTodo)
-        //clear the the input field
-    input.value = ""
-        //Checked button
-    const selectButton = document.createElement('button');
-    selectButton.innerHTML = '<i class="fa-solid fa-check"></i>'
-    selectButton.classList.add('complete-button')
-    todoDiv.appendChild(selectButton)
-        //Trash button
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>'
-    trashButton.classList.add('delete-button')
-    todoDiv.appendChild(trashButton);
-    //add the single list in the div
-    todoList.appendChild(todoDiv)
+        todoDiv.appendChild(newTodo)
+            //clear the the input field
+        input.value = ""
+            //Checked button
+        const selectButton = document.createElement('button');
+        selectButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+        selectButton.classList.add('complete-button')
+        todoDiv.appendChild(selectButton)
+            //Trash button
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+        trashButton.classList.add('delete-button')
+        todoDiv.appendChild(trashButton);
+        //add the single list in the div
+        todoList.appendChild(todoDiv)
+    } else {
+        alert("Todo must be more than 3 character")
+    }
+
+
+    //Create the element and attaching the listener
+})
+
+
+///Delete List and check
+todoList.addEventListener("click", function(e) {
+    const item = e.target;
+    //Delete Item
+    console.log(item);
+    if (item.classList[0] === 'delete-button') {
+        const parentElement = item.parentElement;
+        parentElement.remove()
+    }
+    //CheckMark Item
+    if (item.classList[0] === 'complete-button') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+
+    }
+
 
 })
